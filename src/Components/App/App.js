298aa7b8ab999c2a4db6a2ccbfdd0a57f8765e9e
Braden Bertrand <bradenbertrand@outlook.com>
2019-12-09 03:04:26 -0500

    Update styling
    - Create component directory
    - Add styling to page
    - Imported background photo

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+let logo = ""
+function App() {
+  return (
+    <div>
+      <h1>Spotifi<span class="highlight">zer</span></h1>
+      <div class="App">
+        {/* Add a SearchBar component */}
+    <div class="App-playlist">
+          {/* Add a SearchResults component */}
+      {/* Add a Playlist component */}
+    </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
